de25c9b85f5d746f16a27f7436e221ec645037fc
dibyaranjansathua <dibyaranjan.sathua@gmail.com>
2022-05-10 11:41:48 +0530

    Added the strategy code to main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ Created on:     16/04/22, 12:38 pm
 """
 import argparse
 from src.backtesting.hull_ma_strategy import HullMABackTesting
+from src.strategies.hull_ma_strategy import HullMATradingStrategy
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, required=True, help="Config file path")
+    parser.add_argument("--config", type=str, help="Config file path")
+    parser.add_argument("--trading", action="store_true")
     args = parser.parse_args()
     # config_file_path: str = "/Users/dibyaranjan/Upwork/client_arun_algotrading/HullAlgoTrading/" \
     #                         "data/config.json"
-    HullMABackTesting(config_file_path=args.config).execute()
+    if args.trading:
+        HullMATradingStrategy().execute()
+    else:
+        HullMABackTesting(config_file_path=args.config).execute()
 
 
 if __name__ == "__main__":
